89dc8b00ef145df4d82925e0d201ead3395d74fa
Shravankotagi <your-github-email@gmail.com>
2026-06-01 11:01:13 +0530

    fix PDF download using lazy import approach

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client'],
-  experimental: {
-    optimizePackageImports: ['@react-pdf/renderer'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), '@react-pdf/renderer']
-    }
-    return config
-  },
+  turbopack: {},
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'enlightlab.com' },
